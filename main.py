e914a25dbc65cfa24e59091ff9e318e62704be62
Lars Jacob <jacob.lars@googlemail.com>
2011-06-17 06:46:11 +0200

    Changes multiprocessing.Process infrastructure to multiprocessing.Pool infrastructure

--- main.py
+++ main.py
@@ -19,29 +19,16 @@ def lan(a,t,delta,g,gamma,xi,f):
     return np.array([a0,a1])
 
 
-class RK4Process(mp.Process):
-    result = []
-    
-    queue = Queue.Queue()
-
-    def __init__(self):
-        mp.Process.__init__(self)
-        print "initiate the process"
-    def run(self):
-        print "run the worker"
-        lan, y0, t, delta, g, gamma, xi, f, n = RK4Process.queue.get()
-        print y0, delta, g, gamma
-        data = [t]
-        for i in range(n):
-            k = ku.RK4(lan,y0,t, \
-                        args=(delta,g,gamma,xi,f))
-            # RK4Process.resultLock.acquire()
-            # RK4Process.result.append(k)
-            # RK4Process.resultLock.release()
-            data.append(k)
-        save.save(xi_var,xi_mean,gamma,delta,g,data)
-        print "calling the worker finished"
-        RK4Process.queue.task_done()
+def integrate(lan, y0, t, delta, g, gamma, xi, f, n):
+    print "run the worker"
+    print y0, delta, g, gamma
+    data = [t]
+    for i in range(n):
+        k = ku.RK4(lan,y0,t, \
+                       args=(delta,g,gamma,xi,f))
+        data.append(k)
+    save.save(xi_var,xi_mean,gamma,delta,g,data)
+    print "calling the worker finished"
         
 
 xi_var = 0.001
@@ -60,15 +47,6 @@ g_array = np.arange(0.0,2.0,0.2)
 
 n = 5
 
-print "generate processes"
-worker_processes = [RK4Process() for i in range(4)]
-for worker in worker_processes:
-    print "start next process"
-    worker.start()
+pool = mp.Pool(processes=4)
+result = pool.apply_async(integrate,[(lan,y0,t,g,gamma,xi,f,n) for g in g_array])
 
-for g in g_array:
-    print "put %f into the queue"%g
-    RK4Process.queue.put((lan,y0,delta,g,gamma,xi,f,n))
-    
-print "forked all processes and waiting for the end :)"
-RK4Process.queue.join()
