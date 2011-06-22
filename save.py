58e96a6ad57d7d8fa34ed7f008c24177ceafc645
Lars Jacob <jacob.lars@googlemail.com>
2011-06-22 06:53:02 +0200

    delete old figures and print f()

--- save.py
+++ save.py
@@ -2,10 +2,10 @@ import numpy as np
 import pylab as pl
 import datetime
 
-def save(xi_var,xi_mean,gamma,delta,g,data):
+def save(xi_var,xi_mean,gamma,delta,g,f,data,dir):
     meta = np.array([xi_var,xi_mean,gamma,delta,g])
     timestamp = datetime.datetime.isoformat(datetime.datetime.now())
-    np.savez("data-%s.npz"%timestamp,*data,meta=meta)
+    np.savez("%s/data-%s.npz"%(dir,timestamp),*data,meta=meta)
     for l in data[1:]:
         pl.subplot(211)
         pl.ylabel("$\Re a$")
@@ -16,7 +16,6 @@ def save(xi_var,xi_mean,gamma,delta,g,data):
         pl.plot(data[0],l[:,1])
     #pl.legend()
     pl.subplot(211)
-    pl.title("Symetrisiert: \n  $\\sigma_\\xi = %f,\\; \\bar{\\xi} = %f,\\; \\gamma = %f,\\; \\delta = %f,\\; g = %f$"%(xi_var,xi_mean,gamma,delta,g))
-    pl.savefig("pic-%s.png"%timestamp,format='png')
-
-
+    pl.title("Symetrisiert: \n  $\\sigma_\\xi = %f,\\; \\bar{\\xi} = %f,\\; \\gamma = %f,\\; \\delta = %f,\\; g = %f\\; f = %f$"%(xi_var,xi_mean,gamma,delta,g,f(1)))
+    pl.savefig("%s/pic-%s.png"%(dir,timestamp),format='png')
+    pl.clf()
