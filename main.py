26ea1d97b2aa1fe7416408ecd0b7443c82fe7556
Lars Jacob <jacob.lars@googlemail.com>
2011-06-22 06:54:31 +0200

    non-parallel iteration over various parameters

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import numpy as np
 import pylab as pl
 import kutta as ku
 import save
-import multiprocessing as mp
-import Queue
 
 def lan(a,t,delta,g,gamma,xi,f):
     a0 = -1*delta*a[1]              \
@@ -18,57 +16,39 @@ def lan(a,t,delta,g,gamma,xi,f):
          + xi()
     return np.array([a0,a1])
 
+def run(y0, t, delta, g, gamma, xi_var, f, n):
+    print y0, delta, g, gamma
+    data = [t]
+    xi_mean = 0.0
+    xi = lambda: np.random.normal(xi_mean,xi_var)
 
-class RK4Process(mp.Process):
-    result = []
-    
-    queue = Queue.Queue()
-
-    def __init__(self):
-        mp.Process.__init__(self)
-        print "initiate the process"
-    def run(self):
-        print "run the worker"
-        lan, y0, t, delta, g, gamma, xi, f, n = RK4Process.queue.get()
-        print y0, delta, g, gamma
-        data = [t]
-        for i in range(n):
-            k = ku.RK4(lan,y0,t, \
-                        args=(delta,g,gamma,xi,f))
-            # RK4Process.resultLock.acquire()
-            # RK4Process.result.append(k)
-            # RK4Process.resultLock.release()
-            data.append(k)
-        save.save(xi_var,xi_mean,gamma,delta,g,data)
-        print "calling the worker finished"
-        RK4Process.queue.task_done()
+    for i in range(n):
+        k = ku.RK4(lan,y0,t, \
+                       args=(delta,g,gamma,xi,f))
+        data.append(k)
+    save.save(xi_var,xi_mean,gamma,delta,g,f,data,"data")
         
 
-xi_var = 0.001
-xi_mean = 0.0
-xi = lambda: np.random.normal(xi_mean,xi_var)
+xi_var_array = np.arange(0.1,1.1,0.3)
 
-f = lambda t: 2.0
+f_array = [lambda t: p for p in np.arange(0.0,1.0,0.3)]
 
-gamma = 0.05
-delta = 0.03
+gamma_array = np.arange(0.0,1.0,0.3)
+delta_array = np.arange(0.0,1.0,0.3)
 
-t = np.arange(0,500.0,0.05)
+t = np.arange(0,200.0,0.05)
 y0 = [0.1,0.2]
 
-g_array = np.arange(0.0,2.0,0.2)
+g_array = np.arange(0.0,1.0,0.3)
 
 n = 5
 
-print "generate processes"
-worker_processes = [RK4Process() for i in range(4)]
-for worker in worker_processes:
-    print "start next process"
-    worker.start()
 
 for g in g_array:
-    print "put %f into the queue"%g
-    RK4Process.queue.put((lan,y0,delta,g,gamma,xi,f,n))
+    for xi_var in xi_var_array:
+        for f in f_array:
+            for gamma in gamma_array:
+                for delta in delta_array:
+                    run(y0,t,delta,g,gamma,xi_var,f,n)
     
-print "forked all processes and waiting for the end :)"
-RK4Process.queue.join()
+
