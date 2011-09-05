750412c26937d20a70c7c58b58ea988f4f3f3c9c
Lars Jacob <jacob.lars@googlemail.com>
2011-09-05 20:36:49 -0300

    aufräumen

--- main.py
+++ main.py
@@ -12,18 +12,10 @@ def lan(a,t,delta,g,gamma,xi,f):
         - gamma*a \
         + f(t)  \
         - 1.0j*g*np.abs(a)**2*a 
-        # + np.sqrt(2*gamma)*xi()*(1.0+1.0j)
     return np.array([r.real,r.imag],dtype=np.float64)
 
 def noise(delta,g,gamma,xi,f):
-    # return 0
     return np.sqrt(gamma)*xi()
-
-def oup(a,t,theta,mu,sigma):
-    return theta*(mu-a)
-
-def oup_noise(theta,mu,sigma):
-    return sigma*np.random.normal(0.0,1.0) 
 
 def run(y0, t, btw, delta, g, gamma, f, n = 1):
     print y0, delta, g, gamma
@@ -39,38 +31,21 @@ def run(y0, t, btw, delta, g, gamma, f, n = 1):
     for y in y0:
         for i in range(n):
             print y
-            # k = ku.RK4(lan,y,t,btw,noise, \
-            #              args=(delta,g,gamma,xi,f))
-            # k = integrate.odeint(lan,y,t, \
-            #                        args=(delta,g,gamma,xi,f))
             k = eu.euler(lan,y,t,btw,noise, \
                             args=(delta,g,gamma,xi,f))
-
-            # k = eu.euler(oup,y,t,btw,oup_noise, \
-            #                  args=(2.0,0.0,1.0))
             data.append(k)
     save.save(xi_var,xi_mean,gamma,delta,g,f,data,"data")
         
-# f_array = [lambda t: p for p in np.arange(0.0,1.0,0.3)]
-f_array = [lambda t: 0.3,lambda t: 1.0, lambda t: 3.0]
 f_array = [lambda t: 0.0]
 
-gamma_array = np.arange(0.001,0.1,0.02)
 gamma_array = [0.1]
 
-# delta_array = np.arange(0.0,1.0,0.3)
 delta_array = [0.0]
 
 t = np.arange(0,10.0,0.01)
 btw = 1
 
-# g_array = np.arange(0.0,1.0,0.3)
 g_array = [0.01,0.0]
-y0 = []
-
-# for x in np.arange(-30.0,30.0,5):
-#     for y in np.arange(-30.0,30.0,5):
-#         y0.append((x,y))
 
 y0 = [ (1.0,0.0)]
 n = 1000
--- save.py
+++ save.py
@@ -3,9 +3,16 @@ import pylab as pl
 import datetime
 
 def save(xi_var,xi_mean,gamma,delta,g,f,data,dir):
+    """
+    Trajektorien abspeichern
+    """
+    # Metadaten speichern
     meta = np.array([xi_var,xi_mean,gamma,delta,g,f(1)])
+    # Zeitstempel generieren
     timestamp = datetime.datetime.isoformat(datetime.datetime.now())
+    # Zeitskala, Trajektorien und Metadaten speichern
     np.savez("%s/data-%s.npz"%(dir,timestamp),*data,meta=meta)
+    # Ŭbersichtsplot über Zeitentwicklung plotten und speichern
     for l in data[1:]:
         pl.subplot(211)
         pl.ylabel("$\Re a$")
@@ -14,30 +21,34 @@ def save(xi_var,xi_mean,gamma,delta,g,f,data,dir):
         pl.ylabel("$\Im a$")
         pl.xlabel("t")
         pl.plot(data[0],l[:,1])
-    #pl.legend()
     pl.subplot(211)
     pl.title("Symetrisiert: \n  $\\sigma_\\xi = %f,\\; \\bar{\\xi} = %f,\\; \\gamma = %f,\\; \\delta = %f,\\; g = %f\\; f = %f$"%(xi_var,xi_mean,gamma,delta,g,f(1)))
     pl.savefig("%s/pic-%s.png"%(dir,timestamp),format='png')
     pl.clf()
 
 def load(filename):
+    """
+    Trajektorien laden
+    """
     data = np.load(filename)
     meta = {}
     t = None
     plots = []
     for key in data.keys():
+        # Metadaten extrahieren
         if key == 'meta':
             meta['xi_var'] = data[key][0]
             meta['xi_mean'] = data[key][1]
             meta['gamma'] = data[key][2]
             meta['delta'] = data[key][3]
             meta['g'] = data[key][4]
-            # taking older file versions into account
-            # which didn't save f()
+            # Alte Versionen hatte f() noch nicht gespeichert
             if len(data[key]) == 6:
                 meta['f'] = data[key][5]
+        # Zeitskala extrahieren
         elif key == 'arr_0':
             t = data[key]
+        # Plots extrahieren
         else:
             plots.append(data[key])
             
